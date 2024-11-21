6bed5c25e3af1eb1858a688ca1cf56d5bfc497bd
Nightfury <goldfury.exe@gmail.com>
2024-11-21 17:18:21 +0100

    Create cell.c
    a

---
+++ cell.c
@@ -0,0 +1,35 @@
+//
+// Created by moind on 21/11/2024.
+//
+
+#include "cell.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/***
+ * @brief display a t_cell struct
+ * @param t_cell : cell to display
+ * @return : void
+ */
+void displayCell(t_cell cell) {
+    printf("%d", cell.val);
+}
+
+/***
+ * @brief create a cell, and return a pointer to
+ * @param int value
+ * @return pointer to cell, whit a value, and next is NULL
+ */
+p_cell createCell(int val) {
+    p_cell cell = (p_cell) malloc(sizeof (t_cell));
+    cell->val = val;
+    cell->next = NULL;
+    cell->prec = NULL;
+    return cell;
+}
+
+// Supprime une cellule
+void deleteCell(p_cell cell) {
+    free(cell);
+}
